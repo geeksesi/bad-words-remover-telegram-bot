7c98ade8d1101592aa75662913cb8d449e4ac2e8
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-10-31 12:25:32 +0330

    don't need migration on autoload

--- autoload.php
+++ autoload.php
@@ -5,7 +5,7 @@ include __DIR__ . '/config.php';
 // databases
 include __DIR__ . '/db/ChatsDB.php';
 include __DIR__ . '/db/DB.php';
-include __DIR__ . '/db/migration.php';
+// include __DIR__ . '/db/migration.php';
 
 //words list 
 include __DIR__ . '/badwords.php';
