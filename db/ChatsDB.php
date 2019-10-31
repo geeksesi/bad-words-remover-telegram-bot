d1dc1ab44c6664123a7f0ef435f84381db546c7f
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-10-31 12:25:17 +0330

    finish cli migration

--- db/ChatsDB.php
+++ db/ChatsDB.php
@@ -9,16 +9,17 @@ class ChatsDB
         $this->db = $_db;
     }
 
-    public static function migration($_db, string $_charset, string $_prefix)
+    public static function migration($_db, string $_prefix)
     {
         $prefix = (substr($_prefix, -1) !== "_") ? $_prefix . "_" : $_prefix;
         $table_name = $prefix . "chats";
-        $sql = "CREATE TABLE $table_name (
-            id            INT(4)          NOT NULL AUTO_INCREMENT UNSIGNED ,
-            chat_id       INT(10)         NOT NULL,
-            timestap      BIGINT(12)      NOT NULL UNSIGNED,            
-            PRIMARY KEY  (id)
-          ) $_charset;";
+        $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+            id            INT(4)          UNSIGNED          NOT NULL AUTO_INCREMENT,
+            chat_id       INT(10)                           NOT NULL,
+            chat_type     VARCHAR(30)                       NOT NULL,
+            timestap      BIGINT(12)      UNSIGNED          NOT NULL ,            
+            PRIMARY       KEY (id)
+            )             CHARSET=utf8;";
 
         return $_db->query($sql);
     }
--- db/migration.php
+++ db/migration.php
@@ -3,19 +3,23 @@
 
 function cli_migration()
 {
-    include __DIR__.'/ChatsDB.php';
-    include __DIR__.'/../config.php';
-
+    include __DIR__ . '/../config.php';
+    include __DIR__ . '/ChatsDB.php';
+    echo "\nWellcome to Cli migration\n";
+    echo "remmember if you want drop a table you must do it manualy\n";
     $servername = DB_ADDRESS . ":" . DB_PORT;
     $db =  mysqli_connect($servername, DB_USERNAME, DB_PASSWORD, DB_NAME);
+    if ($db)
+        echo "connect succsessfully to database\n";
+    else
+        die("can't connect to database");
+
+    if (ChatsDB::migration($db, DB_PREFIX))
+        echo "Chats table migration ok\n";
+    else
+        echo "an error happen while want to make chats table\n" . mysqli_error($db);
 }
 
-
-if(defined('STDIN') ) 
-{
+if (defined('STDIN')) {
     cli_migration();
-}
-else 
-{
-
 }
