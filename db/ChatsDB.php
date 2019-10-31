354e6abebd9493f001ee0b79af0edec86fc0d50f
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-10-31 12:46:18 +0330

    ChatsDB query finished

--- db/ChatsDB.php
+++ db/ChatsDB.php
@@ -3,10 +3,11 @@
 class ChatsDB
 {
     private $db;
-
+    public $table_name;
     public function __construct($_db)
     {
         $this->db = $_db;
+        $this->table_name = DB_PREFIX . "chats";
     }
 
     public static function migration($_db, string $_prefix)
@@ -22,5 +23,36 @@ class ChatsDB
             )             CHARSET=utf8;";
 
         return $_db->query($sql);
+    }
+
+    public function add(int $_chat_id, string $_chat_type)
+    {
+        if ($_chat_type !== "group" || $_chat_type !== "“channel”" || $_chat_type !== "“supergroup”" || $_chat_type !== "private") {
+            return false;
+        }
+        $sql_query = "INSERT INTO " . $this->table_name . "(chat_id, chat_type, timestap) VALUES ('" . $_chat_id . "', '" . $_chat_type . "', '" . time() . "')";
+        $result = mysqli_query($this->db, $sql_query);
+        if (!$result)
+            error_log(mysqli_error($this->db));
+
+        return $result;
+    }
+
+    public function get(string $_chat_type = null)
+    {
+        if ($_chat_type !== null) {
+            if ($_chat_type !== "group" || $_chat_type !== "“channel”" || $_chat_type !== "“supergroup”" || $_chat_type !== "private") {
+                return false;
+            }
+        }
+        $where_query = ($_chat_type === null) ? "" : "WHERE chat_type=" . $_chat_type;
+        $sql_query = "SELECT * FROM " . $this->table_name . $where_query;
+
+        $result = mysqli_query($this->db, $sql_query);
+
+        if (!$result)
+            error_log(mysqli_error($this->db));
+
+        return $result;
     }
 }
